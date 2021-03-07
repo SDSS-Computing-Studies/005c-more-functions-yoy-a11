91eb37ae64c3beeabdbd387c095a728f0204237a
yoyo-a11 <yoyol264@deltalearns.ca>
2021-03-06 18:23:33 -0800

    005c

--- task3.py
+++ task3.py
@@ -13,9 +13,9 @@ Sample assertions:
 assert hypotenuse(6,8) == 10
 (2 points)
 """
+import math
 
-
-def hypotenuse():
-    pass
+def hypotenuse(x,y):
+    return math.pow((math.pow(x,2) + math.pow(y,2)),1/2)
 
 assert hypotenuse(6,8) == 10
